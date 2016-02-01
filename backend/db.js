fd6f69279b63a9df7cc76c36409e332f9d51d8b3
Francis Lavoie <lav.francis@gmail.com>
2016-02-01 01:54:56 -0500

    Make database file mac and windows compatible

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,16 @@
 var sqlite3 = require('sqlite3');
+var os = require('os');
+var path = require('path');
+var fs = require('fs');
 
-var FILENAME = '/Users/sanctus/Library/R-Expenses/expenses.sqlite';
+var USERSFILES = os.type() === 'Windows_NT' ? 'AppData/Roaming' : 'Library';
+var FILENAME = 'expenses.sqlite';
+var DIRNAME = path.join(os.homedir(), USERSFILES, 'R-Expenses');
+
+if (!fs.existsSync(DIRNAME)){
+    fs.mkdir(DIRNAME);
+}
 
 module.exports = {
-    sqlite: new sqlite3.Database(FILENAME)
+    sqlite: new sqlite3.Database(path.join(DIRNAME, FILENAME))
 };
