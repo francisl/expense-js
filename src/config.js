1b02f43372b8983fb8ecd00fc2f8ab470df9cd1f
Francis Lavoie <lav.francis@gmail.com>
2016-02-29 05:42:02 -0500

    Updated config to fallback correctly to default when no NODE_ENV

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ var path = require('path');
 var fs = require('fs');
 var process = require('process');
 
-const node_env = process.env['NODE_ENV'] || 'default';
-const file = path.join(process.env.PWD, 'config', process.env['NODE_ENV'] + '.json');
+const NODE_ENV = process.env['NODE_ENV'] || 'default';
+const file = path.join(process.env.PWD, 'config', NODE_ENV + '.json');
 const configJson = fs.readFileSync(file);
 console.log('loading config file');
 const configData = JSON.parse(configJson);
