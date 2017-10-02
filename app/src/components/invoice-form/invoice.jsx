8e4622b99107ac7b40c611f06628bdf7be05cc1f
Francis Lavoie <lav.francis+github@gmail.com>
2017-10-01 23:48:09 -0400

    Fix spenders toggle. Make function instead of class

--- app/src/components/invoice-form/invoice.jsx
+++ app/src/components/invoice-form/invoice.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import { each } from 'lodash';
+
 import { addInvoice, REQUEST_STATUS } from './actions';
 import { DataList } from '../semantic-react/datalist';
 import SpendersList from './spenders-list.jsx';
@@ -17,7 +19,34 @@ class InvoiceForm extends Component {
         this.setInitialFormData();
         this.setCategory = this.setField.bind(this, 'category');
         this.setStore = this.setField.bind(this, 'store');
+        this.updateSpenders = this.updateSpenders.bind(this);
         this.onDateClicked = this.onDateClicked.bind(this);
+        this.toggleSelection = this.toggleSelection.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+      // set default to first spenders if no selection
+      if (nextProps.spenders !== this.props.spenders && nextProps.spenders.length > 0) {
+        nextProps.spenders[0].selected = true;
+      }
+    }
+
+    getSelectedSpenders() {
+        return _.filter(this.props.spenders, (s) => (s.selected === true));
+    }
+
+    toggleSelection(spenderId) {
+      const spender = _.find(this.props.spenders, (s) => {
+        return s.id === parseInt(spenderId);
+      });
+      if (spender) {
+        spender.selected = !spender.selected;
+      }
+    }
+
+    updateSpenders(e) {
+      this.toggleSelection(e.target.value);
+      this.setState({form: {...this.state.form, spenders: this.getSelectedSpenders()}});
     }
 
     setInitialFormData() {
@@ -27,7 +56,6 @@ class InvoiceForm extends Component {
                 store: '',
                 date: this.getToday(),
                 amount: '',
-                spenders: []
             },
             submited: false
         };
@@ -36,28 +64,30 @@ class InvoiceForm extends Component {
 
     addInvoice(e) {
         e.preventDefault()
+        this.state.form.spenders = this.props.spenders.filter(s => {
+          return s.selected === true;
+        }).map(s => (s.id));
+
         this.setState({ ...this.state, submited: true });
         this.props.actions.addInvoice(this.state.form);
     }
 
     getToday(){
         const date = new Date;
-        const singleDigits = ['01', '02', '03', '04', '05', '06', '07', '08', '09'];
         const currMonth = date.getMonth()
-        const month = singleDigits[currMonth] || currMonth+1;
-        const day = singleDigits[date.getDate()] || date.getDate();
-        return date.getFullYear() + '-' + month + '-' + day
-    }
-
-    setField(key, e) {
-        console.log('set field for : ', key, e );
-        this.state.form[key] = e.target.value;
-        this.state.form[key] = e.target.value;
-        this.setState(this.state);
+        const month = currMonth+1; // singleDigits[currMonth] || currMonth+1;
+        const day = date.getDate();
+        return date.getFullYear() + '-' + month + '-' + day;
     }
 
     onDateClicked(d) {
       this.state.form.date = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
+      this.setState(this.state);
+    }
+
+    setField(key, e) {
+      this.state.form[key] = e.target.value;
+      this.state.form[key] = e.target.value;
       this.setState(this.state);
     }
 
@@ -69,10 +99,6 @@ class InvoiceForm extends Component {
         }
     }
 
-    updateSpenders(spenders) {
-        this.setState({form: {...this.state.form, spenders}})
-    }
-
     resetFormWhenNeeded(){
         if (this.state.submited && this.props.form.request.status == REQUEST_STATUS.SUCCESS){
             this.setInitialFormData();
@@ -81,7 +107,8 @@ class InvoiceForm extends Component {
 
     render() {
         this.resetFormWhenNeeded();
-
+        console.log('props spenders : ', this.props.spenders);
+        console.log('spenders : ', this.state.form.spenders);
         return (
             <Layout layout="{width: '236px', padding:'2px'}" vertical center>
                 <StatusMessage request={this.props.form.request}/>
@@ -95,7 +122,10 @@ class InvoiceForm extends Component {
                 <DayPicker value={this.state.form.date} onDayClick={this.onDateClicked} selectedDays={new Date()}/>
                 <InputGroup className={`${Classes.LARGE} large-input`} required type="text" placeholder="Amount" value={this.state.form.amount} onChange={this.setAmount.bind(this)}/>
 
-                <SpendersList className="large-input" spenders={this.props.spenders} selected={this.state.form.spenders} onUpdate={this.updateSpenders.bind(this)}/>
+                <SpendersList
+                  className="large-input"
+                  spenders={this.props.spenders}
+                  onUpdate={this.updateSpenders}/>
 
                 <Button className="large-input" onClick={(e) => this.addInvoice(e)} text="Save" />
 
--- app/src/components/invoice-form/spenders-list.jsx
+++ app/src/components/invoice-form/spenders-list.jsx
@@ -1,35 +1,15 @@
 import React from 'react';
-import { Component } from 'react';
-import _ from 'lodash';
 import { Checkbox } from '@blueprintjs/core';
 
-export default class SpendersList extends Component {
-    constructor(props, context){
-        super(props, context);
-        this.onCheck = this.onCheck.bind(this);
-    }
-
-    getSelectedSpenders(target) {
-        if (target.checked){
-            return  _.concat(this.props.selected, target.value);
-        }
-        return _.filter(this.props.selected, (s) => {
-                return s !== target.value;
-        });
-    }
-
-    onCheck(e) {
-        this.props.onUpdate(this.getSelectedSpenders(e.target));
-    }
-
-    render() {
-      const spenders = this.props.spenders.map((s) => (
-        <Checkbox
-          key={s.id}
-          onChange={this.onCheck}
-          name="spenders"
-          value={s.id}
-          label={s.name} />));
-      return (<div className="large-input">{spenders}</div>);
-    }
+export default (props) => {
+  const spenders = props.spenders.map((s) => (
+    <Checkbox
+      key={s.id}
+      onChange={props.onUpdate}
+      name="spenders"
+      value={s.id}
+      label={s.name}
+      checked={s.selected}
+    />));
+  return (<div className="large-input">{spenders}</div>);
 }
